92ce8bf1908af71c8520a8034364d704ead0ae6a
fabiobf96 <56910403+fabiobf96@users.noreply.github.com>
2020-07-02 19:13:05 +0200

    penso risolti problemi su recycleview

--- MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/RadarChartRow.java
+++ MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/RadarChartRow.java
@@ -1,5 +1,23 @@
 package it.flaviamagnoni.mpandroidchart_t;
 
 public class RadarChartRow {
+    private String mLabel;
+    private int mValue;
 
+    public RadarChartRow(String label, int value){
+        mLabel = label;
+        mValue = value;
+    }
+    public void setLabel(String text){
+        mLabel = text;
+    }
+    public void setValue(int value){
+        mValue = value;
+    }
+    public String getLabel(){
+        return mLabel;
+    }
+    public int getValue(){
+        return mValue;
+    }
 }
