34bb464c43e3687b1b77646f03dc988ec6a3bc3d
fabiobf96 <56910403+fabiobf96@users.noreply.github.com>
2020-07-02 17:45:12 +0200

    casino sulla recycler view

--- MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/BarchartInsertDataActivity.java
+++ MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/BarchartInsertDataActivity.java
@@ -1,6 +1,7 @@
 package it.flaviamagnoni.mpandroidchart_t;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -30,30 +31,57 @@ public class BarchartInsertDataActivity extends AppCompatActivity {
         recyclerView.setAdapter(barCharAdapter);
     }
 
-    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-        @NonNull
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
-        }
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        private String label;
+        private String value;
 
-        @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 0;
-        }
-
-        public class MyViewHolder extends RecyclerView.ViewHolder {
-            private TextView label;
-            private TextView value;
-            public MyViewHolder(@NonNull View itemView) {
-                super(itemView);
-                label = itemView.findViewById(R.id.)
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public static class MyViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView tvLabel;
+            public TextView tvValue;
+            public MyViewHolder(TextView l, TextView v) {
+                super(v, l);
+                tvLabel = l;
+                tvValue = v;
             }
         }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(String newLabel, String newValue) {
+            label = newLabel;
+            value = newValue;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+            // create a new view
+            TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.row_barchart_insert_data, parent, false);
+        ...
+            MyViewHolder vh = new MyViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.textView.setText(mDataset[position]);
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
     }
+
+
 }
