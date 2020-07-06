7624a4bc04dc0654f74595a129e6cf07fa7fc461
GabryLele98 <46486689+GabryLele98@users.noreply.github.com>
2020-07-06 23:44:31 +0200

    Add of "activity_line_chart.xml", "row_linechart_added_data.xml" and "LineChartActivity.java" to complete the Activity that realize a LineChart - Ticket ID: #7

--- MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/LineChartActivity.java
+++ MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/LineChartActivity.java
@@ -9,7 +9,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,13 +19,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/* L'Activity LineChartActivity si occupa di prendere in input i dati inseriti dall'utente,
+ * li mostra in un RecyclerView e, quando l'utente fa touch sul bottone "Create Chart", l'Activity
+ * crea il relativo LineChart.
+ */
 public class LineChartActivity extends AppCompatActivity {
-    private Holder holder;
-    private List<String> myHourDataset, myTemperatureDataset;
+    private Holder holder;  // E' l'oggetto che gestisce l'interfaccia dell'Activity
+    private List<String> myHourDataset, myTemperatureDataset;   // Liste che contengono l'input dell'utente
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,21 +43,51 @@ public class LineChartActivity extends AppCompatActivity {
         myHourDataset = new ArrayList<>();
         myTemperatureDataset = new ArrayList<>();
 
-        holder = new Holder(this);
+        holder = new Holder(this);  // @param Context this: è il contesto in cui gira l'Activity (LineChartActivity)
 
         Intent intent = getIntent();
-//        int lineChartColor = intent.getIntExtra("LineChartColor", 0);  // Recupero dal bundle dell'intent il colore dello sfondo del titolo
+//        int lineChartColor = intent.getIntExtra("LineChartColor", 0);  // Recupero dal bundle dell'intent il colore dello sfondo
 
-//        holder.setColor(/*lineChartColor*/);    // Imposto il colore del titolo, utilizzando l'Holder
+//        holder.setColor(/*lineChartColor*/);    // Imposto il colore dello sfondo del titolo utilizzando l'Holder
     }
 
+    /* deleteItem(int position) rimuove il dato (inserito dall'utente) che si trova nella
+     * posizione "position" e avvisa l'Adapter di aggiornare la RecyclerView.
+     * @param int position: è la posizione nella lista della stringa da rimuovere.
+     */
     public void deleteItem(int position) {
         myHourDataset.remove(position);
         myTemperatureDataset.remove(position);
-        holder.getLineChartAdapter().notifyItemRemoved(position);
+        holder.getLineChartAdapter().notifyItemRemoved(position);   // Avviso l'Adapter che una vista della RecyclerView è stata rimossa
     }
 
+    /* createLineChart(List<String> myHourDataset, List<String> myTemperatureDataset, LineChart lineChart)
+     * è il metodo che si occupa della creazione del LineChart.
+     * @param List<String> myHourDataset: è l'array che contiene i dati delle ore da inserire nell'asse delle x;
+     * @param List<String> myTemperatureDataset: è l'array che contiene i dati delle temperature da inserire nell'asse delle y;
+     * @param LineChart lineChart: è l'oggetto LineChart, collegato all'oggetto nel layout relativo, che deve essere disegnato.
+     */
     private void createLineChart(List<String> myHourDataset, List<String> myTemperatureDataset, LineChart lineChart) {
+        List<String> hourData = myHourDataset, temperatureData = myTemperatureDataset;
+        List<Entry> entries = new ArrayList<Entry>();   // Array di Entry. La classe Entry rappresenta una entry nel chart, con coordinate (x, y)
+        int index;  // Indice che rappresenta la posizione dei dati da trasformare in Entry
+        int labelX, valueY;   // Coordinate dati dell'Entry
+
+        for (index = 0; index < hourData.size(); index++) {
+            // Trasformo i dati da stringa a intero
+            labelX = Integer.parseInt(hourData.get(index));
+            valueY = Integer.parseInt(temperatureData.get(index));
+            // Trasformo i dati in Entry objects
+            entries.add(new Entry(labelX, valueY));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Temperature\nChart");    // Aggiungo le entries al DataSet object, che tiene i dati insieme, e permette di uniformare lo stile
+        //dataSet.setColor();   // Styling
+        //dataSet.setValueTextColor();  // Styling
+
+        LineData lineData = new LineData(dataSet);  // Aggiungo i dataSet objects ad un lineData object, che contiene tutti i dati rappresentati dal LineChart
+        lineChart.setData(lineData);    // Setto il data object nel chart
+        lineChart.invalidate(); // Refresh del LineChart
     }
 
     /* La classe Holder si occuperà della gestione dell'interfaccia dell'Activity.
@@ -59,7 +96,7 @@ public class LineChartActivity extends AppCompatActivity {
     class Holder implements View.OnClickListener {
         private Context context;
         private ConstraintLayout clLineChart;
-        private int color = getColor(R.color.colBtnLineChart);
+        private int color = getColor(R.color.colBtnLineChart);  // Colore che rappresenta la LineChartActivity
 
         private TextView tvLineChartTitle;
         private RecyclerView rvLineChartData;
@@ -68,6 +105,7 @@ public class LineChartActivity extends AppCompatActivity {
         private EditText etlineDataHour, etLineDataTemperature;
         private Button btnAddLineChartData, btnCreateLineChart;
         private LineChart lineChart;
+        private Legend legend;
 
         /* Holder(Context): costruttore;
          * @param context: Context. E' il contesto in cui gira l'activity, in questo caso la MainActivity
@@ -95,14 +133,25 @@ public class LineChartActivity extends AppCompatActivity {
             btnCreateLineChart = findViewById(R.id.btnCreateLineChart);
 
             lineChart = findViewById(R.id.lineChart);
+            lineChart.setBackgroundColor(Color.WHITE);
+            // Imposto la Legend
+            legend = lineChart.getLegend();
+            legend.setForm(Legend.LegendForm.LINE);
+            legend.setTextSize(11f);
+            legend.setTextColor(Color.WHITE);
+            legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+            legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+            legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+            legend.setDrawInside(false);
 
             btnAddLineChartData.setOnClickListener(this);
             btnCreateLineChart.setOnClickListener(this);
         }
 
-        /* setTitleColor(int): void. Metodo che imposta il colore del titolo
-         * @param color: int. E' il valore intero del colore da impostare come sfondo del titolo
-         */
+
+//        /* setColor(int): void. Metodo che imposta il colore del titolo
+//         * @param color: int. E' il valore intero del colore da impostare come sfondo del titolo
+//         */
 //        public void setColor(/*int color*/) {
 //            tvLineChartTitle.setBackgroundColor(getColor(R.color.colBtnLineChart)); // Imposto lo sfondo della TextView
 //            tvLineChartTitle.setTextColor(Color.BLACK); // Imposto il colore del testo della TextView
@@ -110,27 +159,31 @@ public class LineChartActivity extends AppCompatActivity {
 //        }
 
         public RecyclerView.Adapter getLineChartAdapter() {
-            return mAdapter;
+            return mAdapter;    // Ritorna l'Adapter che gestisce la RecyclerView
         }
 
         @Override
         public void onClick(View v) {
             if (v.getId() == R.id.btnAddLineChartData) {
-                if ((etlineDataHour.getText() != null) && (etLineDataTemperature.getText() != null)) {
-                    myHourDataset.add(etlineDataHour.getText().toString());
-                    myTemperatureDataset.add(etLineDataTemperature.getText().toString());
-                    mAdapter.notifyDataSetChanged();
-                    etlineDataHour.setText("");
-                    etLineDataTemperature.setText("");
+                // Verifico che gli input siano corretti (non null e le ore nel range 0 -:- 23)
+                if (((etlineDataHour.getText() != null) && (!etlineDataHour.getText().toString().equals(""))) && ((etLineDataTemperature.getText() != null) && (!etLineDataTemperature.getText().toString().equals("")))) {
+                    if (((Integer.parseInt(etlineDataHour.getText().toString())) >= 0) && ((Integer.parseInt(etlineDataHour.getText().toString())) <= 23)) {
+                        myHourDataset.add(etlineDataHour.getText().toString()); // Aggiungo l'input alla Lista relativa
+                        myTemperatureDataset.add(etLineDataTemperature.getText().toString());   // Aggiungo l'input alla lista relativa
+                        mAdapter.notifyDataSetChanged();    // Notifico all'Adapter che deve aggiungere la vista alla RecyclerView
+                        etlineDataHour.setText("");
+                        etLineDataTemperature.setText("");
+                    }
                 }
             }
             if (v.getId() == R.id.btnCreateLineChart) {
-//                CreateLineChartActivity.createLineChart(context, myHourDataset, myTemperatureDataset, lineChart);
-                createLineChart(myHourDataset, myTemperatureDataset, lineChart);
+                createLineChart(myHourDataset, myTemperatureDataset, lineChart);    // Chiamo il metodo per creare il LineChart
             }
         }
     }
 
+    /* MyAdapter si occupa della gestione delle viste ViewHolder della RecyclerView
+     */
     public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         List<String> mHourDataset, mTemperatureDataset;
 
@@ -139,6 +192,9 @@ public class LineChartActivity extends AppCompatActivity {
             mTemperatureDataset = myTemperatureDataset;
         }
 
+        /* onCreateViewHolder() viene chiamato quando la RecyclerView necessita di creare una
+         * nuova riga (ViewHolder), che viene riempita nella onBindViewHolder()
+         */
         @NonNull
         @Override
         public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -147,17 +203,28 @@ public class LineChartActivity extends AppCompatActivity {
             return new ViewHolder(cl);
         }
 
+        /* onBindViewHolder() utilizza la posizione della ViewHolder da riempire, per determinare
+         * quali dovrebbero essere i contenuti, in base alla posizione nella lista.
+         * @param ViewHolder viewHolder: è la vista che deve essere riempita;
+         * @param int position: è la posizione degli oggetti nella lista
+         */
         @Override
         public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
             viewHolder.tvLineHourData.setText("Hour: " + mHourDataset.get(position));
             viewHolder.tvLineTemperatureData.setText("Temperature: " + mTemperatureDataset.get(position));
         }
 
+        /* getItemCount() restituisce la dimensione del Dataset.
+
+         */
         @Override
         public int getItemCount() {
             return mHourDataset.size();
         }
 
+        /* La classe ViewHolder ha il compito di gestire l'interfaccia delle view
+         * che costituiscono una singola riga della RecyclerView
+         */
         class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
             private TextView tvLineHourData, tvLineTemperatureData;
             private ImageView ivDeleteLineChartData;
@@ -174,7 +241,7 @@ public class LineChartActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (v.getId() == R.id.ivDeleteLineChartData) {
-                    deleteItem(getAdapterPosition());
+                    deleteItem(getAdapterPosition());   // Chiamo il metodo deleteItem() per rimuovere l'oggetto dalla lista e dalla RecyclerView
                 }
             }
         }
