a7b74a4b3945d9de8037ea2584fb02486bcb462e
GabryLele98 <46486689+GabryLele98@users.noreply.github.com>
2020-07-08 19:28:03 +0200

    Added comments (as javadoc documentation) - Ticket ID: #9

--- MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/LineChartActivity.java
+++ MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/LineChartActivity.java
@@ -1,3 +1,9 @@
+/*
+ * In questa Activity viene ricevuto l'intent che ha lanciato l'Activity e vengono ricavati gli
+ * array passati tramite il bundle dell'Intent. In questo modo è possibile costruire i dati del
+ * chart e creare il relativo LineChart.
+ */
+
 package it.flaviamagnoni.mpandroidchart_t;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,53 +23,75 @@ import com.github.mikephil.charting.data.LineDataSet;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * L'Activity LineChartActivity si occupa della gestione dei dati raccolti (inseriti dall'utente
+ * nell'Activity chiamante) e della creazione del relativo LineChart.
+ *  * @Author EndGame()
+ *  * @Version 1.0
+ */
 public class LineChartActivity extends AppCompatActivity {
-    private Holder holder;
-    private ArrayList<Integer> hourDataset, temperatureDataset;
+    private Holder holder;  // Gestore dell'interfaccia
+    private ArrayList<Integer> hourDataset, temperatureDataset; // ArrayList di interi che contengono l'input, inserito dall'utente, e passato tramite Intent
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_chart);
 
-        holder = new Holder(this);
+        holder = new Holder(this);  // @param this: è il contesto in cui gira l'Activity (LineChartActivity)
 
-        Intent intent = getIntent();
-        hourDataset = intent.getIntegerArrayListExtra("hourDataset");
-        temperatureDataset = intent.getIntegerArrayListExtra("temperatureDataset");
+        Intent intent = getIntent();    // Intent che contiene nel bundle i dati inseriti dall'utente nell'Activity chiamante
+        hourDataset = intent.getIntegerArrayListExtra("hourDataset");   // hourDataset contiene l'array di label inserite dall'utente
+        temperatureDataset = intent.getIntegerArrayListExtra("temperatureDataset"); // temperatureDataset contiene l'array di value inseriti dall'utente
 
-        createLineChart();
+        createLineChart();  // Metodo per creare il LineChart
     }
 
+    /**
+     * createLineChart() è il metodo che si occupa della costruzione dei dati da visualizzare
+     * nel LineChart.
+     */
     public void createLineChart() {
-        List<Entry> entries = new ArrayList<Entry>();
-        int index;
-        int hourX, temperatureY;
+        List<Entry> entries = new ArrayList<Entry>();   // Array di Entry. La classe Entry rappresenta una entry nel chart, con coordinate (x, y)
+        int index;  // Indice che rappresenta la posizione dei dati, nell'array, da trasformare in Entry
+        int hourX, temperatureY;    // Coordinate dati dell'Entry
 
         for (index = 0; index < hourDataset.size(); index++) {
-            hourX = hourDataset.get(index);
-            temperatureY = temperatureDataset.get(index);
-            entries.add(new Entry(hourX, temperatureY));
+            hourX = hourDataset.get(index); // Prendo dall'array la label in posizione index
+            temperatureY = temperatureDataset.get(index);   // Prendo dall'array il value in posizione index
+            entries.add(new Entry(hourX, temperatureY));    // Trasformo i dati in Entry objects e li aggiungo all'array di Entry (entries)
         }
 
-        LineDataSet dataSet = new LineDataSet(entries, "");
+        LineDataSet dataSet = new LineDataSet(entries, ""); // Aggiungo le entries al DataSet object, che tiene i dati insieme, e permette di uniformare lo stile
         //dataSet.setColor();   // Styling
         //dataSet.setValueTextColor();  // Styling
 
-        LineData lineData = new LineData(dataSet);
+        LineData lineData = new LineData(dataSet);  // Aggiungo i dataSet objects ad un lineData object, che contiene tutti i dati rappresentati dal LineChart
 
-        holder.createChart(lineData);
+        holder.createChart(lineData);   // Chiama il metodo dell'Holder che aggiorna e disegna il LineChart
     }
 
+    /**
+     * La classe Holder si occupa della gestione dell'interfaccia dell'Activity.
+     * Utilizzo il paradigma Model view viewControl
+     * @Author EndGame()
+     * @Version 1.0
+     */
     class Holder {
-        private Context context;
-        private int lineChartActivityColor = getColor(R.color.colBtnLineChart);
+        private Context context;    // Rappresenta il contesto in cui l'Activity gira (LineChartActivity)
+        private int lineChartActivityColor = getColor(R.color.colBtnLineChart); // Colore che rappresenta il LineChart
+
+        // Definizione delle View e ViewGroup che realizzano il Layout
         private ConstraintLayout clLineChartActivity;
         private TextView tvLineChartTitle;
 
-        private LineChart lineChart;
+        private LineChart lineChart;    // Questa View è il LineChart
 
+        /**
+         * Holder(Context) è il costruttore. Qui vengono collegate le viste nel Layout
+         * ("activity_line_chart.xml) al codice Java.
+         * @param context: Context. E' il contesto in cui gira l'activity, in questo caso la LineChartActivity.
+         */
         public Holder(Context context) {
             this.context = context;
 
@@ -77,9 +105,14 @@ public class LineChartActivity extends AppCompatActivity {
             lineChart.setBackgroundColor(Color.WHITE);
         }
 
+        /**
+         * createChart metodo dell'Holder che carica i dati nel chart,
+         * aggiorna e disegna il LineChart.
+         * @param lineData : data object che deve essere settato nel chart.
+         */
         public void createChart(LineData lineData) {
-            lineChart.setData(lineData);
-            lineChart.invalidate();
+            lineChart.setData(lineData);    // Setto il data object lineData nel chart
+            lineChart.invalidate(); // Refresh del LineChart
         }
     }
 }
--- MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/LineChartInsertDataActivity.java
+++ MidTerm/MPAndroidChartt/app/src/main/java/it/flaviamagnoni/mpandroidchart_t/LineChartInsertDataActivity.java
@@ -1,3 +1,10 @@
+/*
+ * In questa Activity si fa un esempio di come si può costruire un LineChart.
+ * Scopo dell'esempio: realizzare un grafico che mostra l'andamento della temperatura nelle ore
+ * inserite dall'utente (hour, temperature). Si può, per esempio, visualizzare l'andamento della
+ * temperatura di una giornata.
+ */
+
 package it.flaviamagnoni.mpandroidchart_t;
 
 import androidx.annotation.NonNull;
@@ -20,29 +27,43 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+/**
+ * L'Activity LineChartInsertDataActivity si occupa di gestire la fase di presa in input dei dati
+ * inseriti dall'utente e, tramite l'Holder li invia in un RecyclerView e, quando l'utente fa touch
+ * sul bottone "Create Chart", viene lanciata la relativa Activity che crea il LineChart con
+ * i dati ottenuti.
+ * @Author EndGame()
+ * @Version 2.0
+ */
 public class LineChartInsertDataActivity extends AppCompatActivity {
-    private Holder holder;
-    private ArrayList<Integer> mLineChartDataset, mHourDataset, mTemperatureDataset;
+    private Holder holder;  // E' l'oggetto che gestisce l'interfaccia dell'Activity
+    private ArrayList<Integer> mHourDataset, mTemperatureDataset;    // Array di interi che contengono l'input dell'utente
     private Intent intent;
-    private int latestHour = 0;
+    private int latestHour = 0; // Ultimo input Label inserito (Hour)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_chart_insert_data);
 
-        holder = new Holder(this);
-        mLineChartDataset = new ArrayList<>();
-        mHourDataset = new ArrayList<>();
-        mTemperatureDataset = new ArrayList<>();
+        holder = new Holder(this);  // @param Context this: è il contesto in cui gira l'Activity (LineChartInsertDataActivity)
+        mHourDataset = new ArrayList<>();   // Creo l'array (inizialmente array vuoto) che conterrà le input label (Hour)
+        mTemperatureDataset = new ArrayList<>();    // Creo l'array (inizialmente array vuoto) che conterrà gli input value (Temperature)
 
         intent = getIntent();
     }
 
+    /**
+     * La classe Holder si occupa della gestione dell'interfaccia dell'Activity.
+     * Utilizzo il paradigma Model view viewControl
+     * @Author EndGame()
+     * @Version 1.0
+     */
     class Holder implements View.OnClickListener {
-        private Context context;
-        private int lineChartcolor = getColor(R.color.colBtnLineChart);  // Colore che rappresenta la LineChartActivity
+        private Context context;    // Rappresenta il contesto in cui l'Activity gira (LineChartInsertDataActivity)
+        private int lineChartcolor = getColor(R.color.colBtnLineChart);  // Colore che rappresenta il LineChart
 
+        // Definizione delle View e ViewGroup che realizzano il Layout
         private ConstraintLayout clLineChartInsertData;
 
         private TextView tvLineChartTitle;
@@ -51,8 +72,13 @@ public class LineChartInsertDataActivity extends AppCompatActivity {
 
         private RecyclerView rvInsertLineChartData;
         private RecyclerView.LayoutManager layoutManager;
-        private RecyclerView.Adapter mAdapter;
+        private RecyclerView.Adapter mAdapter;  // si occupa della gestione delle viste ViewHolder della RecyclerView
 
+        /**
+         * Holder(Context) è il costruttore. Qui vengono collegate le viste nel Layout
+         * ("activity_line_chart_insert_data.xml) al codice Java.
+         * @param context: Context. E' il contesto in cui gira l'activity, in questo caso la LineChartInsertDataActivity.
+         */
         public Holder(Context context) {
             this.context = context;
             clLineChartInsertData = findViewById(R.id.clLineChartInsertData);
@@ -80,6 +106,12 @@ public class LineChartInsertDataActivity extends AppCompatActivity {
             btnCreateLineChart.setOnClickListener(this);
         }
 
+        /**
+         * onClick() è l'evento associato al click di una View. Qui si gestisce il click del bottone
+         * che aggiunge i dati inseriti dall'utente ai Dataset e alla RecyclerView, e il click
+         * del bottone che lancia l'Activity che crea il LineChart.
+         * @param v : è la View che è stata cliccata
+         */
         @Override
         public void onClick(View v) {
             if (v.getId() == R.id.btnAddLineChartData) {
@@ -89,9 +121,9 @@ public class LineChartInsertDataActivity extends AppCompatActivity {
                 if (((etlineDataHour.getText() != null) && (!hour.equals(""))) && ((etLineDataTemperature.getText() != null) && (!temperature.equals("")))) {
                     if (((Integer.parseInt(hour)) >= 0) && ((Integer.parseInt(hour)) <= 23)) {
                         if (Integer.parseInt(hour) >= latestHour) {
-                            mHourDataset.add(Integer.parseInt(hour)); // Aggiungo l'input alla Lista relativa
-                            mTemperatureDataset.add(Integer.parseInt(temperature));   // Aggiungo l'input alla lista relativa
-                            latestHour = Integer.parseInt(hour);
+                            mHourDataset.add(Integer.parseInt(hour)); // Aggiungo l'input hour alla Lista relativa
+                            mTemperatureDataset.add(Integer.parseInt(temperature));   // Aggiungo l'input temperature alla lista relativa
+                            latestHour = Integer.parseInt(hour);    // Aggiorno con l'ultimo input hour inserito
                             mAdapter.notifyDataSetChanged();    // Notifico all'Adapter che deve aggiungere la vista alla RecyclerView
                             etlineDataHour.setText("");
                             etLineDataTemperature.setText("");
@@ -100,26 +132,47 @@ public class LineChartInsertDataActivity extends AppCompatActivity {
                 }
             }
             if (v.getId() == R.id.btnCreateLineChart) {
-                Intent lineChartIntent = new Intent(context, LineChartActivity.class);
-                lineChartIntent.putExtra("hourDataset", mHourDataset);
-                lineChartIntent.putExtra("temperatureDataset", mTemperatureDataset);
-                startActivity(lineChartIntent);
+                Intent lineChartIntent = new Intent(context, LineChartActivity.class);  // Intent che lancia l'Activity LineChartActivity.class
+                lineChartIntent.putExtra("hourDataset", mHourDataset);  // Put dell'array di dati (mHourDataset) nel bundle dell'Intent
+                lineChartIntent.putExtra("temperatureDataset", mTemperatureDataset);    // Put dell'array di dati (mTemperatureDataset) nel bundle dell'Intent
+                startActivity(lineChartIntent); // Lancio l'Activity LineChartActivity.class che si occupa della creazione del chart
             }
         }
 
+        /**
+         * getLineChartAdapter() ritorna l'Adapter associato al RecyclerView
+         * @return RecyclerView.Adapter : l'adapter che gestisce il RecyclerView
+         */
         public RecyclerView.Adapter getLineChartAdapter() {
-            return mAdapter;
+            return mAdapter;    // Ritorna l'Adapter che gestisce la RecyclerView
         }
     }
 
+    /**
+     * MyLineChartAdapter si occupa della gestione delle viste ViewHolder della RecyclerView
+     * @Author EndGame()
+     * @Version 2.0
+     */
     public class MyLineChartAdapter extends RecyclerView.Adapter<MyLineChartAdapter.ViewHolder> {
-        ArrayList<Integer> hourDataset, temperatureDataset;
+        ArrayList<Integer> hourDataset, temperatureDataset; // Array di interi che contengono l'input dell'utente
 
+        /**
+         * MyLineChartAdapter è il costruttore della classe.
+         * @param myHourDataset : è l'array contenente le input label (Hour);
+         * @param myTemperatureDataset : è l'array contenente gli input value (Temperature).
+         */
         MyLineChartAdapter(ArrayList<Integer> myHourDataset, ArrayList<Integer> myTemperatureDataset) {
             this.hourDataset = myHourDataset;
             this.temperatureDataset = myTemperatureDataset;
         }
 
+        /**
+         * onCreateViewHolder() viene chiamato quando la RecyclerView necessita di creare una
+         * nuova riga (ViewHolder), che viene riempita nella onBindViewHolder().
+         * @param parent : ViewGroup
+         * @param viewType : tipo di view
+         * @return ViewHolder : oggetto gestito dalla classe ViewHolder (che si occupa del Layout della row del RecyclerView)
+         */
         @NonNull
         @Override
         public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -128,21 +181,43 @@ public class LineChartInsertDataActivity extends AppCompatActivity {
             return new ViewHolder(cl);
         }
 
+        /**
+         * onBindViewHolder() utilizza la posizione della ViewHolder da riempire, per determinare
+         * quali dovrebbero essere i contenuti, in base alla posizione nella lista.
+         * @param viewHolder : è la vista che deve essere riempita;
+         * @param position : è la posizione degli oggetti nella lista.
+         */
         @Override
         public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
-            viewHolder.tvLineHourData.setText("Hour: " + mHourDataset.get(position));
-            viewHolder.tvLineTemperatureData.setText("Temperature: " + mTemperatureDataset.get(position));
+            viewHolder.tvLineHourData.setText("Hour: " + mHourDataset.get(position));   // Set del testo da mostrare nella TextView relativa alla label Hour
+            viewHolder.tvLineTemperatureData.setText("Temperature: " + mTemperatureDataset.get(position));  // Set del testo da mostrare nella TextView relativa al value Temperature
         }
 
+        /**
+         * getItemCount() restituisce la dimensione del Dataset.
+         * @return int : dimensione del dataset
+         */
         @Override
         public int getItemCount() {
             return mHourDataset.size();
         }
 
+        /**
+         * La classe ViewHolder ha il compito di gestire l'interfaccia delle View (ViewHolder)
+         * che costituiscono una singola riga della RecyclerView.
+         * @Author EndGame()
+         * @Version 1.0
+         */
         class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            // Definizione delle View che realizzano il Layout
             private TextView tvLineHourData, tvLineTemperatureData;
             private ImageView ivDeleteLineChartData;
 
+            /**
+             * ViewHolder() è il costruttore. Qui vengono collegate le viste ViewHolder
+             * (nel Layout "row_linechart_added_data.xml") al codice Java.
+             * @param cl : è il ConstraintLayout (ViewGroup)
+             */
             public ViewHolder(ConstraintLayout cl) {
                 super(cl);
                 tvLineHourData = cl.findViewById(R.id.tvLineHourData);
@@ -152,18 +227,29 @@ public class LineChartInsertDataActivity extends AppCompatActivity {
                 ivDeleteLineChartData.setOnClickListener(this);
             }
 
+            /**
+             * onClick() è l'evento associato al click di una View. Qui si gestisce il click della
+             * ImageView "ic_delete.xml" che permette l'eliminazione della riga dai Dataset e
+             * dalla RecyclerView.
+             * @param v : è la View che ha generato l'evento.
+             */
             @Override
             public void onClick(View v) {
                 if (v.getId() == R.id.ivDeleteLineChartData) {
-                    deleteItem(getAdapterPosition());   // Chiamo il metodo deleteItem() per rimuovere l'oggetto dalla lista e dalla RecyclerView
+                    deleteItem(getAdapterPosition());   // Chiamo il metodo deleteItem() per rimuovere l'oggetto dalla lista e dalla RecyclerView che sta nella posizione getAdapterPosition()
                 }
             }
         }
     }
 
+    /**
+     * deleteItem(int position) rimuove il dato (inserito dall'utente) che si trova nella
+     * posizione "position" e avvisa l'Adapter di aggiornare la RecyclerView.
+     * @param adapterPosition : è la posizione, nella lista, del dato da rimuovere.
+     */
     private void deleteItem(int adapterPosition) {
-        mHourDataset.remove(adapterPosition);
-        mTemperatureDataset.remove(adapterPosition);
+        mHourDataset.remove(adapterPosition);   // Rimuove, dall'array mHourDataset, il dato con indice adapterPosition
+        mTemperatureDataset.remove(adapterPosition);    // Rimuove, dall'array mTemperatureDataset, il dato con indice adapterPosition
         holder.getLineChartAdapter().notifyItemRemoved(adapterPosition);   // Avviso l'Adapter che una vista della RecyclerView è stata rimossa
     }
 }
