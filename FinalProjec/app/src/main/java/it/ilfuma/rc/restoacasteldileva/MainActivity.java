144109f1b4e4dbe3943ef21cec504ed5648382d0
fabiobf96 <56910403+fabiobf96@users.noreply.github.com>
2020-07-15 16:58:20 +0200

    controllare perche non riesco a fare il get dal server rest

--- FinalProjec/app/src/main/java/it/ilfuma/rc/restoacasteldileva/MainActivity.java
+++ FinalProjec/app/src/main/java/it/ilfuma/rc/restoacasteldileva/MainActivity.java
@@ -2,13 +2,30 @@ package it.ilfuma.rc.restoacasteldileva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
+        setContentView(R.layout.activity_main);
+        new Holder();
+    }
+    private class Holder implements View.OnClickListener {
+        private Button btnCategories;
+        Holder(){
+            btnCategories = findViewById(R.id.btnCategories);
+            btnCategories.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
+            startActivity(intent);
+        }
     }
 }
